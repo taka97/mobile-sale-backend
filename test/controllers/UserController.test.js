706a928e2de41966c6023fd53b76a333c45bc9ed
Ho Van Hoang <vanhoang0609@gmail.com>
2019-11-22 02:34:38 +0700

    add testcase

--- test/controllers/UserController.test.js
+++ test/controllers/UserController.test.js
@@ -3,12 +3,12 @@ import { expect } from 'chai';
 import app from '../../src/app';
 import User from '../../src/models/user';
 
-const sampleUserData = {
+const sampleUserData = [{
   email: 'vanhoang0609@gmail.com',
   password: 'Abc12345',
   name: 'Ho Van Hoang',
   phone: '0987654321'
-}
+}]
 
 describe('User Controller', () => {
   before('***Cleaning user collection', async () => {
@@ -16,12 +16,21 @@ describe('User Controller', () => {
   });
 
   describe('#Create', () => {
-    it('create a user', async () => {
-      const response = await request(app).post('/api/users').send(sampleUserData);
+    it('Create a user - 1', async () => {
+      const response = await request(app).post('/api/users').send(sampleUserData[0]);
       expect(response.status).equal(201);
-      expect(response.body.data).to.have.property('email', sampleUserData.email);
-      expect(response.body.data).to.have.property('name', sampleUserData.name);
-      expect(response.body.data).to.have.property('phone', sampleUserData.phone);
+      expect(response.body.data).to.have.property('email', sampleUserData[0].email);
+      expect(response.body.data).to.have.property('name', sampleUserData[0].name);
+      expect(response.body.data).to.have.property('phone', sampleUserData[0].phone);
+      expect(response.body.data).to.not.have.property('password');
+    });
+
+    it('Create a user - 2', async () => {
+      const response = await request(app).post('/api/users').send(sampleUserData[1]);
+      expect(response.status).equal(201);
+      expect(response.body.data).to.have.property('email', sampleUserData[1].email);
+      expect(response.body.data).to.have.property('name', sampleUserData[1].name);
+      expect(response.body.data).to.have.property('phone', sampleUserData[1].phone);
       expect(response.body.data).to.not.have.property('password');
     });
   });
