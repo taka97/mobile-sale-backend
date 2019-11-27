daa0b7256e6afedeafdcfe9d829a1f18953537b3
Ho Van Hoang <vanhoang0609@gmail.com>
2019-11-28 03:09:16 +0700

    Fix restrictPermission

--- src/middlewares/restrictPermission.js
+++ src/middlewares/restrictPermission.js
@@ -1,22 +1,15 @@
 import createError from 'http-errors';
 
-const restrictPermission = (roles) => {
-  if (!roles) {
-    roles = []
-  };
-
-  if (typeof roles !== string && !Array.isArray(roles)) {
-    return next(createError());
-  }
-
-  if (!Array.isArray(roles)) {
-    roles = [roles];
-  }
-
-  console.dir(roles);
+const restrictPermission = (...roles) => {
+  let availableForAll = roles.length === 0;
 
   return function (req, res, next) {
-
+    const { user } = req;
+    if (!availableForAll && !roles.includes(user.roles)) {
+      return next(createError(401, 'You don\'t have permission to access'));
+    } else {
+      return next();
+    }
   };
 };
 
