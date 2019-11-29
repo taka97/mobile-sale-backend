34763c6b015dd6a516bd983fcb7a179e83c54e58
Ho Van Hoang <vanhoang0609@gmail.com>
2019-11-30 02:20:35 +0700

    Add filter utils

---
+++ src/utils/filter-select.js
@@ -0,0 +1,16 @@
+import debug from 'debug';
+import _ from 'lodash';
+
+const dg = debug('MS::utils::filterSelect');
+
+function filterSelect(select, allowField, excludeField) {
+  if (!allowField && !excludeField) return select;
+  let field = _.pull(allowField, ...excludeField);
+  dg(field);
+  let result = _.intersection(Array.isArray(select) ? select : [select], field);
+
+  return result.length === 0 ? field : result;
+}
+
+export default filterSelect;
+
