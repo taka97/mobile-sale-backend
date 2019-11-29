a9df592adb700108beb13d386cf25c284c1262ad
Ho Van Hoang <vanhoang0609@gmail.com>
2019-11-30 06:50:46 +0700

    Add utils select field

---
+++ src/utils/select.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+// Return a function that filters a result object or array
+// and picks only the fields passed as `params.query.$select`
+// and additional `otherFields`
+function select (params, ...otherFields) {
+  const fields = params && params.query && params.query.$select;
+
+  if (Array.isArray(fields) && otherFields.length) {
+    fields.push(...otherFields);
+  }
+
+  const convert = result => {
+    if (!Array.isArray(fields)) {
+      return result;
+    }
+
+    return _.pick(result, ...fields);
+  };
+
+  return result => {
+    if (Array.isArray(result)) {
+      return result.map(convert);
+    }
+
+    return convert(result);
+  };
+};
+
+export default select;
