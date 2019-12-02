24aed514d01ed7b75913901ec4721379695ed819
Ho Van Hoang <vanhoang0609@gmail.com>
2019-12-02 11:19:46 +0700

    Add middleware validator

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,2 +1,3 @@
 export * from './authenticate';
 export * from './restrictPermission';
+export * from './validatorData';
---
+++ src/middlewares/validatorData.js
@@ -0,0 +1,31 @@
+import createError from 'http-errors';
+import { BADREQUEST } from '../helpers/http-error';
+
+/**
+ * Middleware for validate data
+ * @param {Function} validator validator
+ * @param {string} setField `req[setField] = true` when validate success
+ */
+function validatorData(validator, setField) {
+  if (typeof validator !== 'function') {
+    throw new Error('validator must be a function (validatorData)');
+  }
+
+  if (setField && typeof setField !== 'string') {
+    throw new Error('setField must be a string (validatorData)');
+  }
+
+  return (req, res, next) => {
+    const { error } = validator(req.body);
+    if (error) {
+      return next(createError(BADREQUEST, error.details[0].message));
+    }
+    if (setField) {
+      req[setField] = true;
+    }
+    next();
+  };
+}
+
+/* eslint-disable import/prefer-default-export */
+export { validatorData }
