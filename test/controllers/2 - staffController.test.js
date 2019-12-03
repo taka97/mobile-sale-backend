8a4f70bc3a384d5855e5815347ed9a5d1568becc
Ho Van Hoang <vanhoang0609@gmail.com>
2019-12-04 01:02:33 +0700

    Add testcase for staffController

--- test/controllers/2 - staffController.test.js
+++ test/controllers/2 - staffController.test.js
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { expect } from 'chai';
 import app from '../../src/app';
 import User from '../../src/models/user';
+import { forEachAsync } from '../../src/utils';
 
 const sampleStaff = [
   {
@@ -32,22 +33,63 @@ const sampleStaff = [
     password: 'Abc12345',
     birthDate: '2019/10/05',
     sex: 'male',
+    storeId: '507f1f77bcf86cd799439011',
+  },
+  {
+    email: 'vanhoang0609@yahoo.com',
+    fullname: 'Van Hoang',
+    password: 'Abc12345',
+    birthDate: '2019/10/05',
+    sex: 'male',
+    storeId: '507f1f77bcf86cd799439011',
   },
 ];
 
-const sampleStaffData = {
-  email: 'vanhoang0609@gmail.com',
-  fullname: 'Van Hoang',
-  password: 'Abc12345',
+const sampleCustomerData = {
+  email: 'customer@gmail.com',
+  fullname: 'Customer',
+  username: 'customer',
+  password: 'customer',
   birthDate: '2019/10/05',
   sex: 'male',
+  roles: 'customer',
+};
+
+const sampleStaffData = {
+  email: 'staff@gmail.com',
+  fullname: 'Staff',
+  username: 'staff',
+  password: 'staff',
+  birthDate: '2019/10/05',
+  sex: 'male',
+  roles: 'staff',
+  storeId: '507f1f77bcf86cd799439010',
+};
+
+const sampleAdminData = {
+  email: 'admin@gmail.com',
+  fullname: 'Admin',
+  username: 'admin',
+  password: 'admin',
+  birthDate: '2019/10/05',
+  sex: 'male',
+  roles: 'admin',
+};
+
+const newData = {
+  email: 'newstaff@gmail.com',
+  fullname: 'New Staff',
+  username: 'newstaff',
+  passsword: 'newpassword',
+  birthDate: '1997/10/06',
+  sex: 'male',
+  roles: 'admin',
+  phone: '0123456789',
+  cmnd: '215416497',
+  address: 'my address',
 };
 
 describe('Staff Controller', () => {
-  let accessToken;
-  let userId;
-
-  // eslint-disable-next-line func-names
   before('***Cleaning user collection', async () => {
     await User.deleteMany();
   });
@@ -120,27 +162,52 @@ describe('Staff Controller', () => {
     });
   });
 
-  describe('#Get a user detail', () => {
+  describe.only('#Get a user detail', () => {
     const noExistId = '5de25b1504da0435c8e714ad';
+    const accessToken = {};
+    const userId = {};
 
     before(async () => {
+      let data;
       await User.deleteMany();
+      await User.deleteMany();
+      await User.create(sampleAdminData);
       await User.create(sampleStaffData);
+      await User.create(sampleCustomerData);
+      await User.create({ ...sampleStaff[5], roles: 'staff' });
+      await User.create({ ...sampleStaff[6], roles: 'staff' });
 
-      const response = await request(app)
-        .post('/api/authentication')
-        .send({
-          email: sampleStaffData.email,
-          password: sampleStaffData.password,
-        });
-      accessToken = response.body.accessToken;
-      userId = response.body.userId;
+      data = [sampleStaff[5], sampleStaff[6]];
+      await forEachAsync(data, async (current, index) => {
+        const response = await request(app)
+          .post('/api/authentication')
+          .send({
+            email: current.email,
+            password: current.password,
+            strategy: 'staff',
+          });
+        userId[`Staff0${index}`] = response.body.userId;
+        accessToken[`Staff0${index}`] = response.body.accessToken;
+      });
+
+      data = [sampleAdminData, sampleStaffData, sampleCustomerData];
+      await forEachAsync(data, async (current) => {
+        const response = await request(app)
+          .post('/api/authentication')
+          .send({
+            email: current.email,
+            password: current.password,
+            strategy: current.roles,
+          });
+        userId[current.roles] = response.body.userId;
+        accessToken[current.roles] = response.body.accessToken;
+      });
     });
 
     it('should return Id invalid with Bearer Token in Authorization header', async () => {
       const response = await request(app)
         .get('/api/staffs/abc')
-        .set('Authorization', `Bearer ${accessToken}`);
+        .set('Authorization', `Bearer ${accessToken.staff}`);
       expect(response.status).to.equal(400);
       expect(response.body.message).to.be.a('string').include('"Id" is invalid');
     });
@@ -148,7 +215,7 @@ describe('Staff Controller', () => {
     it('should return Id invalid with Token in Authorization header', async () => {
       const response = await request(app)
         .get('/api/staffs/abc')
-        .set('Authorization', `jwt ${accessToken}`);
+        .set('Authorization', `jwt ${accessToken.staff}`);
       expect(response.status).to.equal(400);
       expect(response.body.message).to.be.a('string').include('"Id" is invalid');
     });
@@ -156,7 +223,7 @@ describe('Staff Controller', () => {
     it('should return No record with Bearer Token in Authorization header', async () => {
       const response = await request(app)
         .get(`/api/staffs/${noExistId}`)
-        .set('Authorization', `Bearer ${accessToken}`);
+        .set('Authorization', `Bearer ${accessToken.staff}`);
       expect(response.status).to.equal(404);
       expect(response.body.message).to.be.a('string')
         .include(`No record found for id '${noExistId}'`);
@@ -165,7 +232,7 @@ describe('Staff Controller', () => {
     it('should return No record with Token in Authorization header', async () => {
       const response = await request(app)
         .get(`/api/staffs/${noExistId}`)
-        .set('Authorization', `jwt ${accessToken}`);
+        .set('Authorization', `jwt ${accessToken.staff}`);
       expect(response.status).to.equal(404);
       expect(response.body.message).to.be.a('string')
         .include(`No record found for id '${noExistId}'`);
@@ -173,8 +240,8 @@ describe('Staff Controller', () => {
 
     it('should return user data with Bearer Token in Authorization header', async () => {
       const response = await request(app)
-        .get(`/api/staffs/${userId}`)
-        .set('Authorization', `Bearer ${accessToken}`);
+        .get(`/api/staffs/${userId.staff}`)
+        .set('Authorization', `Bearer ${accessToken.staff}`);
       expect(response.status).to.equal(200);
       expect(response.body).to.have.property('_id');
       expect(response.body).to.not.have.property('password');
@@ -182,13 +249,13 @@ describe('Staff Controller', () => {
       expect(response.body).to.have.property('email', sampleStaffData.email);
       expect(response.body).to.have.property('birthDate',
         (new Date(sampleStaffData.birthDate)).toISOString());
-      expect(response.body).to.have.property('sex', sampleStaff[5].sex);
+      expect(response.body).to.have.property('sex', sampleStaffData.sex);
     });
 
     it('should return user data with Token in Authorization header', async () => {
       const response = await request(app)
-        .get(`/api/staffs/${userId}`)
-        .set('Authorization', `jwt ${accessToken}`);
+        .get(`/api/staffs/${userId.staff}`)
+        .set('Authorization', `jwt ${accessToken.staff}`);
       expect(response.status).to.equal(200);
       expect(response.body).to.have.property('_id');
       expect(response.body).to.not.have.property('password');
@@ -196,7 +263,51 @@ describe('Staff Controller', () => {
       expect(response.body).to.have.property('email', sampleStaffData.email);
       expect(response.body).to.have.property('birthDate',
         (new Date(sampleStaffData.birthDate)).toISOString());
-      expect(response.body).to.have.property('sex', sampleStaff[5].sex);
+      expect(response.body).to.have.property('sex', sampleStaffData.sex);
+    });
+
+    it.skip('should return not authentication with user as customer', async () => {
+      const response = await request(app)
+        .get(`/api/staffs/${userId.customer}`)
+        .set('Authorization', `jwt ${accessToken.customer}`);
+      expect(response.status).to.equal(401);
+      expect(response.body).to.have.property('message', 'Unauthorized');
+    });
+
+    it.skip('should return success with user as admin', async () => {
+      const response = await request(app)
+        .get(`/api/staffs/${userId.admin}`)
+        .set('Authorization', `jwt ${accessToken.admin}`);
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property('_id');
+      expect(response.body).to.not.have.property('password');
+      expect(response.body).to.have.property('fullname', sampleAdminData.fullname);
+      expect(response.body).to.have.property('email', sampleAdminData.email);
+      expect(response.body).to.have.property('birthDate',
+        (new Date(sampleAdminData.birthDate)).toISOString());
+      expect(response.body).to.have.property('sex', sampleAdminData.sex);
+    });
+
+    it.skip('shound return success with owner as staff same at store', async () => {
+      const response = await request(app)
+        .get(`/api/staffs/${userId.admin}`)
+        .set('Authorization', `jwt ${accessToken.admin}`);
+      expect(response.status).to.equal(200);
+      expect(response.body).to.have.property('_id');
+      expect(response.body).to.not.have.property('password');
+      expect(response.body).to.have.property('fullname', sampleAdminData.fullname);
+      expect(response.body).to.have.property('email', sampleAdminData.email);
+      expect(response.body).to.have.property('birthDate',
+        (new Date(sampleAdminData.birthDate)).toISOString());
+      expect(response.body).to.have.property('sex', sampleAdminData.sex);
+    });
+
+    it.skip('shound return not found with staff diff store', async () => {
+      const response = await request(app)
+        .get(`/api/staffs/${userId.customer}`)
+        .set('Authorization', `jwt ${accessToken.customer}`);
+      expect(response.status).to.equal(404);
+      expect(response.body).to.have.property('message', 'Not found');
     });
   });
 });
