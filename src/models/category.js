f1f026b18a2b019d84c1fb51aae2325e9e0c91e3
Ho Van Hoang <vanhoang0609@gmail.com>
2019-12-12 18:36:43 +0700

    Lint code

--- src/models/category.js
+++ src/models/category.js
@@ -5,13 +5,11 @@ const CategorySchema = new Schema(
     name: {
       type: String,
       required: true,
-    }
+    },
   },
   {
     timestamps: true,
   },
 );
-
-CategorySchema.index({ roles: 1, isDeleted: 1 }, { background: true });
 
 export default model('Category', CategorySchema);
