ae64ec84397d2f5de4b0cf661de859607d796a40
Ho Van Hoang <vanhoang0609@gmail.com>
2019-12-23 19:40:11 +0700

    Docs: add doc for comment router

--- src/routes/comment.js
+++ src/routes/comment.js
@@ -34,10 +34,81 @@ const middlewareForShow = [
 
 const router = Router();
 
+/**
+ * @swagger
+ * /comments:
+ *  post:
+ *    tags:
+ *      - 'comment'
+ *    summary: 'Create new comment'
+ *    produces:
+ *      - 'application/json'
+ *    parameters:
+ *      - in: body
+ *        name: body
+ *        description: 'Id of product'
+ *        required: true
+ *        schema:
+ *          $ref: '#/definitions/Comment'
+ *    responses:
+ *      201:
+ *        description: Create comment is success
+ *        schema:
+ *          $ref: '#/definitions/Comment'
+ */
 router.post('/', middlewareForCreate, CommentController.create);
 
+/**
+ * @swagger
+ * /comments:
+ *  get:
+ *    tags:
+ *      - 'comment'
+ *    summary: 'Get list of comment'
+ *    produces:
+ *      - 'application/json'
+ *    parameters:
+ *      - in: query
+ *        name: productId
+ *        description: 'Id of product'
+ *        required: true
+ *      - in: query
+ *        name: parentId
+ *        description: 'If of parent comment'
+ *    responses:
+ *      201:
+ *        description: List all of comment
+ *        schema:
+ *          $ref: '#/definitions/QueryResponse'
+ */
 router.get('/', middlewareForIndex, CommentController.index);
 
+/**
+ * @swagger
+ * /comment/{commentId}:
+ *  get:
+ *    tags:
+ *      - 'comment'
+ *    summary: 'Get detail of comment'
+ *    produces:
+ *      - 'application/json'
+ *    parameters:
+ *      - in: path
+ *        name: commentId
+ *        description: 'Id of comment'
+ *        required: true
+ *        schema:
+ *          type: byte
+ *    responses:
+ *      200:
+ *        description: Detail of comment
+ *        schema:
+ *          $ref: '#/definitions/CommentResponse'
+ *      401:
+ *        description: You donn't have permission to access
+ *      404:
+ *        description: No record found for id `{commentId}`
+ */
 router.get('/:id', middlewareForShow, CommentController.show);
 
 // router.patch('/:id', middlewareForPatch, CommentController.patch);
