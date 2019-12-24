19bf5522cc93e645ac88f4e6b699e5ee290c3b9d
Ho Van Hoang <vanhoang0609@gmail.com>
2019-12-25 01:32:23 +0700

    Wrap middleware restrictToOwner in function

--- src/middlewares/restrictToOwner.js
+++ src/middlewares/restrictToOwner.js
@@ -1,12 +1,14 @@
 import { Unauthorized } from 'http-errors';
 
-function restrictToOwner(req, res, next) {
-  const { _id: userId } = req.user;
+function restrictToOwner() {
+  return function (req, res, next) {
+    const { _id: userId } = req.user;
 
-  if (req.params.id !== userId.toString()) {
-    return next(new Unauthorized('You don\'t have permission to modify'));
-  }
-  return next();
+    if (req.params.id !== userId.toString()) {
+      return next(new Unauthorized('You don\'t have permission to modify'));
+    }
+    return next();
+  };
 }
 
 /* eslint-disable import/prefer-default-export */
